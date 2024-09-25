34bd32f1ea99be1207dc3039e1362ab71c4b2119
miruakasha19272 <miruakasha5@gmail.com>
2024-09-25 14:34:48 +0800

    credentials.js

---
+++ src/credentials.js
@@ -0,0 +1,16 @@
+// Facebook log-in credentials for bot account
+exports.FACEBOOK_EMAIL = "09301859862";
+exports.FACEBOOK_PASSWORD = "Mark15";
+// Heroku application token
+exports.TOKEN = "";
+// MemCachier credentials (from MemCachier dashboard)
+exports.MEMCACHIER_PASSWORD = "";
+exports.MEMCACHIER_SERVERS = "";
+exports.MEMCACHIER_USERNAME = "";
+// Spotify API credentials
+exports.SPOTIFY_CLIENTID = "";
+exports.SPOTIFY_CLIENTSECRET = "";
+// Wolfram short-answer API key
+exports.WOLFRAM_KEY = "";
+// Open Weather Map API key
+exports.WEATHER_KEY = "";
